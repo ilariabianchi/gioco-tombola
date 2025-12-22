a50ed48fcb59dc55ddb5d5f6455ac41b5b22464f
ilaria <ilariabianchi2010@gmail.com>
2025-12-22 15:09:28 +0100

    creazione tabellone numeri

--- gioco-tombola.cpp
+++ gioco-tombola.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <time.h>
 using namespace std;
+
+int main(){
+
+for(int i=1; i<91; i++){
+    while (i<11){
+    	if(i==1){
+    		cout<<i<<"  ";
+			i++;	
+		}
+	cout<<" "<<i<<"  ";
+	i++;
+		if(i==11){
+			cout<<endl<<endl;
+		}
+	}
+    cout<<i<<"  ";
+    if(i%10==0){
+        cout<<endl<<endl;;
+    }
+	}
+    
+    return 0;
+}
+
